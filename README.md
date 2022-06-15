4256355f6605b00d7c21711d3b4bff2514c07eb5
airmanxtw <airmanx@stust.edu.tw>
2022-06-15 15:59:28 +0800

    add example

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # vue2-youtube-embeder
 
 [demo](https://airmanxtw.github.io/vue2-youtube-embeder/)
+
+## example 1
+```html
+<div
+      style="
+        display: flex;
+        width: 100%;
+        height: 320px;
+        flex-direction: column;
+        background-color: white;
+        overflow: hidden;
+        border-radius: 15px !important;
+      "
+    >
+      <vue2-youtube-embeder
+        src="https://www.youtube.com/embed/LOxxhecSHQM"
+        title="GOBELINS"
+        autoplay
+        loop
+      ></vue2-youtube-embeder>
+    </div>
+```
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,32 @@
 <template>
-    <div>
-        <h1>video example</h1>
-        <div style="display: flex; width: 100%; height: 320px; flex-direction: column; background-color: white; overflow: hidden;border-radius: 15px !important;">
-            <vue2-youtube-embeder src="https://www.youtube.com/embed/LOxxhecSHQM" title="GOBELINS" autoplay loop></vue2-youtube-embeder>
-        </div>
-        <div> 
-            bottom
-        </div>        
+  <div>
+    <h1>example 1</h1>   
+    <div
+      style="
+        display: flex;
+        width: 100%;
+        height: 320px;
+        flex-direction: column;
+        background-color: white;
+        overflow: hidden;
+        border-radius: 15px !important;
+      "
+    >
+      <vue2-youtube-embeder
+        src="https://www.youtube.com/embed/LOxxhecSHQM"
+        title="GOBELINS"
+        autoplay
+        loop
+      ></vue2-youtube-embeder>
     </div>
-    
+    <div>bottom</div>
+  </div>
 </template>
 <script>
 import Vue2YoutubeEmbeder from "@/components/Vue2YoutubeEmbeder.vue";
 export default {
-    components:{
-        Vue2YoutubeEmbeder
-    }
-}
+  components: {
+    Vue2YoutubeEmbeder,
+  },
+};
 </script>
