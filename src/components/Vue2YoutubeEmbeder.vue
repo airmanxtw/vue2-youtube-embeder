69d324ccadc4a01b27a5d50ac30df65f5db52224
airmanxtw <airmanx@stust.edu.tw>
2022-06-15 15:39:27 +0800

    init

---
+++ src/components/Vue2YoutubeEmbeder.vue
@@ -0,0 +1,138 @@
+<template>
+  <div :class="fillMode ? 'videoWrapper':''">
+    <iframe
+      ref="videoIframe"
+      :width="width"
+      :height="height"
+      :src="youtubeSrc"
+      :title="title"
+      frameborder="0"
+      :allow="allow"
+      :allowfullscreen="allowfullscreen"
+      :style="{ top: iframeTop }"
+    ></iframe>
+    <div v-if="fillMode"
+      class="foreground-text textStyle"
+      :style="{ top: titleTop }"
+    >
+      <div v-if="!!title">{{ title }}</div>
+      <div v-if="allowfullscreen">
+        <button @click="fullscreen">Full Screen</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    width: {
+      type: String,
+      default: "100%",
+    },
+    height: {
+      type: String,
+      default: "700",
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    allow: {
+      type: String,
+      default:
+        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+    },
+    allowfullscreen: {
+      type: Boolean,
+      default: true,
+    },
+    autoplay: {
+      type: Boolean,
+      default: false,
+    },
+    loop: {
+      type: Boolean,
+      default: false,
+    },
+    src: {
+      type: String,
+      default: null,
+    },
+    iframeTop: {
+      type: String,
+      default: "-25%",
+    },
+    titleTop: {
+      type: String,
+      default: "5%",
+    },
+    fillMode:{
+      type:Boolean,
+      default:true
+    }
+  },
+  data() {
+    return {
+      model: false,
+    };
+  },
+  computed: {
+    youtubeSrc() {
+      var url = new URL(this.src);
+      var code = this.src.split("/").at(-1);
+      if (this.autoplay) url.searchParams.append("autoplay", 1);
+      if (this.loop) {
+        url.searchParams.append("loop", 1);
+        url.searchParams.append("playlist", code);
+      }
+      url.searchParams.append("mute", 1);
+      return url.toString();
+    },
+  },
+  methods: {
+    fullscreen() {
+      var e = this.$refs.videoIframe;
+      if (e.requestFullscreen) {
+        e.requestFullscreen();
+      } else if (e.webkitRequestFullscreen) {
+        e.webkitRequestFullscreen();
+      } else if (e.mozRequestFullScreen) {
+        e.mozRequestFullScreen();
+      } else if (e.msRequestFullscreen) {
+        e.msRequestFullscreen();
+      }
+    },
+  },
+};
+</script>
+<style>
+.videoWrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  /* 16:9 */
+  padding-top: 0px;
+  height: 0;
+}
+
+.videoWrapper iframe {
+  position: absolute;
+  /* top: -25%; */
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px !important;
+}
+
+.foreground-text {
+  position: absolute;
+  top: 50%;
+  left: 2%;
+  width: 100%;
+}
+
+.textStyle{
+    text-align: left;
+    font-size: x-large;
+    color:white;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Vue2YoutubeEmbeder from "./Vue2YoutubeEmbeder.vue";
+Vue2YoutubeEmbeder.install = function (Vue) { Vue.component('vue2-youtube-embeder', Vue2YoutubeEmbeder) }
+export default Vue2YoutubeEmbeder;
